81d06115cb3d4f0fe93a4a06e684e92e8bea9625
analistacomercialrbn <analistacomercialrbn@gmail.com>
2026-09-17 14:53:45 -0300

    Ativa expansão da tabela dinâmica

--- supervisao/gestao_page_compat.py
+++ supervisao/gestao_page_compat.py
@@ -1,7 +1,9 @@
 from gestao_page_compat_base import aplicar_formatacao_comparativos as _aplicar_base
 from gestao_layout_patch import aplicar_layout_simplificado
+from gestao_expand_patch import aplicar_expandir_tabela
 
 
 def aplicar_formatacao_comparativos():
     _aplicar_base()
     aplicar_layout_simplificado()
+    aplicar_expandir_tabela()
