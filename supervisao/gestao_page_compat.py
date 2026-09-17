f6cf913521d25f2a70326c4dec38cf70c54f90b9
analistacomercialrbn <analistacomercialrbn@gmail.com>
2026-09-17 14:55:52 -0300

    Aplica tela cheia ao modal da tabela dinâmica

--- supervisao/gestao_page_compat.py
+++ supervisao/gestao_page_compat.py
@@ -1,9 +1,11 @@
 from gestao_page_compat_base import aplicar_formatacao_comparativos as _aplicar_base
 from gestao_layout_patch import aplicar_layout_simplificado
 from gestao_expand_patch import aplicar_expandir_tabela
+from gestao_fullscreen_css import aplicar_fullscreen_dialog
 
 
 def aplicar_formatacao_comparativos():
     _aplicar_base()
     aplicar_layout_simplificado()
     aplicar_expandir_tabela()
+    aplicar_fullscreen_dialog()
