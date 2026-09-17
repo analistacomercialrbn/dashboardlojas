f155c885d7e59eb8b191074c4fbf119b01268cd3
analistacomercialrbn <analistacomercialrbn@gmail.com>
2026-09-17 16:52:12 -0300

    Aplica novo visual na etapa de supervisores

--- supervisao/gestao_page_compat.py
+++ supervisao/gestao_page_compat.py
@@ -5,6 +5,7 @@ from gestao_expand_patch import aplicar_expandir_tabela
 from gestao_fullscreen_css import aplicar_fullscreen_dialog
 from gestao_sugestao_ciclo_reapply import aplicar_sugestao_ciclo_estavel
 from gestao_wizard_visual import aplicar_visual_wizard_metas
+from gestao_supervisor_visual import aplicar_visual_supervisores
 
 
 def aplicar_formatacao_comparativos():
@@ -15,3 +16,4 @@ def aplicar_formatacao_comparativos():
     aplicar_fullscreen_dialog()
     aplicar_sugestao_ciclo_estavel()
     aplicar_visual_wizard_metas()
+    aplicar_visual_supervisores()
